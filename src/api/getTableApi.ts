2f00fd4788b6b00b366fa825d7f2ccac827f9e8d
mityaycho <mityaycho@gmail.com>
2020-07-25 20:09:46 +0300

    add some changes

--- src/api/getTableApi.ts
+++ src/api/getTableApi.ts
@@ -4,10 +4,10 @@ import instance from './api';
 export const getTableApi = {
 	getTableSmall() {
 		return instance.get('?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}/')
-		.then(res => res);
+		.then(res => res.data);
 	},
 	getTableLarge() {
 		return instance.get('?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}/')
-		.then(res => res);
+		.then(res => res.data);
 	}
 };
