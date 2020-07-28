1aaec77ce3049e4bdf2df125ad1b95c4e5dc0e8b
mityaycho <mityaycho@gmail.com>
2020-07-28 21:46:56 +0300

    add MaterialTable

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,39 @@ import { useSelector } from 'react-redux';
 import { AppStateType } from './redux/store';
 import Header from './components/Header';
 import logo from './logo.svg';
+import MaterialTableJSX from './components/MaterialTableJSX';
 
 
 
 function App() {
 
 	const [showTable, setShowTable] = useState('');
+	const [tableWatch, setTableWatch] = useState('');
 	const { data: tableData, preloader } = useSelector((state: AppStateType) => state.tablePage);
 
 	return (
 		<div className="container App">
-			<Header setShowTable={setShowTable} showTable={showTable} />
-			{preloader ?
-				<img src={logo} className="preloader-logo" alt="logo" /> :
-				showTable && <Table tableData={tableData} />
+			<button
+				type="button"
+				className="btn btn-secondary btn-sm m-2"
+				onClick={() => setTableWatch('material-table')}>Material table</button>
+
+			<button
+				type="button"
+				className="btn btn-secondary btn-sm m-2"
+				onClick={() => setTableWatch('custom-table')}>Custom table</button>
+			{tableWatch === 'material-table' && <MaterialTableJSX />}
+
+			{tableWatch === 'custom-table' &&
+				<div>
+					<Header setShowTable={setShowTable} showTable={showTable} />
+					{preloader ?
+						<img src={logo} className="preloader-logo" alt="logo" /> :
+						showTable && <Table tableData={tableData} />
+					}
+				</div>
 			}
+
 		</div>
 	);
 };
