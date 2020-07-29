a9a821d4a61a9c9c13f58f04e858e2088d56b2db
mityaycho <mityaycho@gmail.com>
2020-07-29 21:11:28 +0300

    add some changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 
-const instance = axios.create({ baseURL: ' http://www.filltext.com/' });
+const instance = axios.create({ baseURL: 'http://www.filltext.com/' });
 
 export default instance;
